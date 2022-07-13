204997d994d1f05cf05611d50f6a98b3982d90df
LeonardoReisC <leozinho848@gmail.com>
2022-07-13 15:46:22 -0300

    Trabalhando com Interfaces e Classes Abstratas

---
+++ Maven_28_06/src/main/java/exercicio2/model/animais/AnimalDeEstimacao.java
@@ -0,0 +1,5 @@
+package exercicio2.model.animais;
+
+public interface AnimalDeEstimacao {
+    public void brincar();
+}
---
+++ Maven_28_06/src/main/java/exercicio2/model/animais/Gato.java
@@ -0,0 +1,18 @@
+package exercicio2.model.animais;
+
+public class Gato extends Animal implements AnimalDeEstimacao{
+    public Gato() { this(""); }
+
+    public Gato(String nome) { super(nome, 4); }
+    @Override
+    public void brincar() {
+        System.out.println(nome + " brinca com bola de meia.");
+    }
+
+    @Override
+    public void caminhar() {
+        System.out.println(nome + " anda com 4 pernas.");
+    }
+    @Override
+    public void comer() { System.out.println(nome + " come racao."); }
+}
